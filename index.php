26aab20356ddde988e69941904cd9176aae222f2
ACER\Angga <anggap820@gmail.com>
2018-02-26 09:58:59 +0700

    test 11

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = 'a306aa56-c732-48c2-b19e-adaf07d772ec'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
